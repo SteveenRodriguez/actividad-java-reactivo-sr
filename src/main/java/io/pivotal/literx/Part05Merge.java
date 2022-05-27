05b3b00ff277b73f7b5dba1b1444b25a00371723
steveen <ss.rodriguez00021@gmail.com>
2022-05-27 01:11:56 -0500

    parte 05 Merge resuelta

--- src/main/java/io/pivotal/literx/Part05Merge.java
+++ src/main/java/io/pivotal/literx/Part05Merge.java
@@ -14,22 +14,25 @@ public class Part05Merge {
 //========================================================================================
 
 	// TODO Merge flux1 and flux2 values with interleave
+	// Combinar los valores flux1 y flux2 con intercalado
 	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		return flux1.mergeWith(flux2);
 	}
 
 //========================================================================================
 
 	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
+	//Combine los valores de flux1 y flux2 sin intercalar (valores de flux1 y luego valores de flux2)
 	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		return Flux.mergeSequential(flux1, flux2);
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux containing the value of mono1 then the value of mono2
+	// Cree un flujo que contenga el valor de mono1 y luego el valor de mono2
 	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		return Flux.concat(mono1, mono2);
 	}
 
 }
